6e81a02098734a1d3e0c82a9869628d095a8359b
Asmi Dagar <asmidagar68@gmail.com>
2026-02-28 10:28:23 +0530

    28-02-2026

---
+++ simplifyPath.py
@@ -0,0 +1,17 @@
+#Leetcode 71
+
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        path = path.split('/')
+        stack = []
+
+        for val in path:
+            if val == "" or val == '.':
+                continue
+            elif val == '..':
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(val)
+
+        return '/' + ('/'.join(stack))
